4a8d60da39fddbc34f05c501d3ec5a418a77de96
fatiademinas <239409198+rrnrodrigo@users.noreply.github.com>
2025-10-24 15:36:51 -0300

    corrige estrutura de rotas

---
+++ src/app/api/trpc/route.ts
@@ -0,0 +1,15 @@
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { appRouter } from "../../../server/routers";
+
+export const runtime = "edge";
+
+const handler = (req: Request) => {
+  return fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req,
+    router: appRouter,
+    createContext: () => ({}),
+  });
+};
+
+export { handler as GET, handler as POST };
