ee3596ca0099bdedd9a2b3607783d58f8831c633
fatiademinas <239409198+rrnrodrigo@users.noreply.github.com>
2026-01-01 23:11:08 -0300

    deletando arquivos antigos

---
+++ api/lista_produtos.ts
@@ -0,0 +1,14 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import postgres from 'postgres';
+
+const { POSTGRES_URL } = process.env;
+const sql = postgres(POSTGRES_URL!, { ssl: 'require' });
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  try {
+    const produtos = await sql`SELECT * FROM produtos`;
+    return res.status(200).json(produtos);
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+}
