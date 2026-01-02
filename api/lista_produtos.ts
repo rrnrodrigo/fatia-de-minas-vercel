0a979a7caf739c4473bd8cac3cfb48efdb9a0795
fatiademinas <239409198+rrnrodrigo@users.noreply.github.com>
2026-01-01 23:22:04 -0300

    Update lista_produtos.ts

--- api/lista_produtos.ts
+++ api/lista_produtos.ts
@@ -6,7 +6,7 @@ const sql = postgres(POSTGRES_URL!, { ssl: 'require' });
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
-    const produtos = await sql`SELECT * FROM produtos`;
+    const produtos = await sql`SELECT * FROM products`;
     return res.status(200).json(produtos);
   } catch (error: any) {
     return res.status(500).json({ error: error.message });
