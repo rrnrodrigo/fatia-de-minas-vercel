83ead7d40c373b875b836902a27338dd2babf7b0
fatiademinas <239409198+rrnrodrigo@users.noreply.github.com>
2026-01-06 21:56:01 -0300

    index vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: './', // Define que o index.html está na raiz
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    assetsDir: 'assets',
   },
 });
